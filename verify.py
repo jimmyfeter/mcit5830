7e52eff713e533982d51ad6b13495afff5f2f4a5
jimmyfeter <97856176+jimmyfeter@users.noreply.github.com>
2024-11-04 14:37:05 -0500

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,45 +1,65 @@
 from web3 import Web3
-from eth_account.messages import encode_defunct
-import random
+import eth_account
+import os
 
-def signChallenge( challenge ):
-
-    w3 = Web3()
-
-    #This is the only line you need to modify
-
-    #generate ethereum key pair
-
-    sk = "0xdba1bdad6ce33c9ea799b188c1a26fb0d5e5258dd9dd121c1b89c553c6627c50"
-
-    acct = w3.eth.account.from_key(sk)
-
-    signed_message = w3.eth.account.sign_message( challenge, private_key = acct._private_key )
-
-    return acct.address, signed_message.signature
-
-
-def verifySig():
+def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
     """
-        This is essentially the code that the autograder will use to test signChallenge
-        We've added it here for testing 
+    Generate a stable private key
+    challenge - byte string
+    keyId (integer) - which key to use
+    filename - filename to read and store mnemonics
+
+    Each mnemonic is stored on a separate line
+    If fewer than (keyId+1) mnemonics have been generated, generate a new one and return that
     """
-
-    challenge_bytes = random.randbytes(32)
-
-    challenge = encode_defunct(challenge_bytes)
-    address, sig = signChallenge( challenge )
-
-    w3 = Web3()
-
-    return w3.eth.account.recover_message( challenge , signature=sig ) == address
-
-if __name__ == '__main__':
-    """
-        Test your function
-    """
-    if verifySig():
-        print( f"You passed the challenge!" )
+     # Read mnemonics from the specified file
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            mnemonics = f.readlines()
     else:
-        print( f"You failed the challenge!" )
+        mnemonics = []
+    w3 = Web3()
 
+
+    msg = eth_account.messages.encode_defunct(challenge)
+
+    # #generate account from the mnemonic of the given key id
+    # if keyId < len(mnemonics):
+    #     mnemonic = mnemonics[keyId].strip()
+    # else:
+    #     raise ValueError("Insufficient mnemonics in the key id.")
+
+    # #generate private key and account from mnemonic
+    # acct = eth_account.Account.from_mnemonic(mnemonic)
+    # eth_addr = acct.address
+
+    # #sign the challenge
+    # sig = acct.sign_message(msg)
+
+    #generate account from the mnemonic of the given key id
+    if keyId < len(mnemonics):
+        mnemonic = mnemonics[keyId].strip()
+    else:
+        #create a new mnemonic and write to file 
+        acct = Account.create()
+        mnemonic = acct.key.hex() 
+        with open(filename, 'a') as f:
+            f.write(mnemonic + '\n')
+
+    #generate private key and account from mnemonic
+    acct = Account.from_key(mnemonic)
+    eth_addr = acct.address
+
+    # Sign the challenge
+    sig = acct.sign_message(msg)
+
+    assert eth_account.Account.recover_message(msg,signature=sig.signature.hex()) == eth_addr, f"Failed to sign message properly"
+
+    #return sig, acct #acct contains the private key
+    return sig, eth_addr
+
+if __name__ == "__main__":
+    for i in range(4):
+        challenge = os.urandom(64)
+        sig, addr= get_keys(challenge=challenge,keyId=i)
+        print( addr )
