62cd6cedc81b25327612d3ceab1860f4756c5900
jimmyfeter <97856176+jimmyfeter@users.noreply.github.com>
2024-11-05 12:15:27 -0500

    Update verify.py

--- verify.py
+++ verify.py
@@ -1,43 +1,45 @@
 from web3 import Web3
-from eth_account import Account
 from eth_account.messages import encode_defunct
-import os
+import random
 
-def get_keys(challenge):
+def signChallenge( challenge ):
+
+    w3 = Web3()
+
+    #This is the only line you need to modify
+
+    #generate ethereum key pair
+
+    sk = "0xdba1bdad6ce33c9ea799b188c1a26fb0d5e5258dd9dd121c1b89c553c6627c50"
+
+    acct = w3.eth.account.from_key(sk)
+
+    signed_message = w3.eth.account.sign_message( challenge, private_key = acct._private_key )
+
+    return acct.address, signed_message.signature
+
+
+def verifySig():
     """
-    Generate an Ethereum account from a mnemonic and sign a message.
+        This is essentially the code that the autograder will use to test signChallenge
+        We've added it here for testing 
     """
 
-    #endpoints
-    BSC_RPC_URL = "https://bsc-dataseed.binance.org/"
-    AVAX_RPC_URL = "https://api.avax.network/ext/bc/C/rpc"
+    challenge_bytes = random.randbytes(32)
 
-    #initialize Web3 instances 
-    bsc_w3 = Web3(Web3.HTTPProvider(BSC_RPC_URL))
-    avax_w3 = Web3(Web3.HTTPProvider(AVAX_RPC_URL))
+    challenge = encode_defunct(challenge_bytes)
+    address, sig = signChallenge( challenge )
 
-    #verify connections
-    if not bsc_w3.isConnected() or not avax_w3.isConnected():
-        raise ConnectionError("Failed to connect to one or both networks.")
+    w3 = Web3()
 
-    #your mnemonic phrase
-    mnemonic = "sorry proof update famous swear soldier bullet upset lake solar deny fat"
-    
-    #create account from mnemonic
-    web3 = Web3()
-    web3.eth.account.enable_unaudited_hdwallet_features()
-    acct = web3.eth.account.from_mnemonic(my_mnemonic)
-    eth_addr = acct.address
+    return w3.eth.account.recover_message( challenge , signature=sig ) == address
 
-    #encode and sign
-    msg = encode_defunct(challenge)
-    sig = acct.sign_message(msg)
+if __name__ == '__main__':
+    """
+        Test your function
+    """
+    if verifySig():
+        print( f"You passed the challenge!" )
+    else:
+        print( f"You failed the challenge!" )
 
-    
-    return sig, eth_addr
-
-if __name__ == "__main__":
-    for i in range(4):
-        challenge = os.urandom(64)
-        sig, addr = get_keys(challenge=challenge)
-        print(addr)
