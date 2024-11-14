5f85d152d3b42f0f2b758d72d56587088a428439
jimmyfeter <97856176+jimmyfeter@users.noreply.github.com>
2024-11-14 17:41:48 -0500

    Update Destination.sol

--- Destination.sol
+++ Destination.sol
@@ -27,7 +27,7 @@ contract Destination is AccessControl {
     
         require(underlying_tokens[_underlying_token] == address(0), "Token already exists");
 
-        BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol);
+        BridgeToken newToken = new BridgeToken(_underlying_token, name, symbol, manager);
         address wrappedTokenAddr = address(newToken);
 
         underlying_tokens[_underlying_token] = wrappedTokenAddr;
